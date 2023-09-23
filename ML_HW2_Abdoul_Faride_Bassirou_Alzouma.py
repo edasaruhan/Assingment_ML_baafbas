8faa8f3d349580761d9fbf62a41cc6c62c280570
ABDOUL FARIDE BASSIROU ALZOUMA <abdoulfaridbassirou7898@gmail.com>
2023-09-23 23:24:48 +0300

    training function is added

--- ML_HW2_Abdoul_Faride_Bassirou_Alzouma.py
+++ ML_HW2_Abdoul_Faride_Bassirou_Alzouma.py
@@ -17,3 +17,20 @@ def cost_func(y_true,y_pred):
 	epsilon = 1e-15
 	y_pred = np.clip(y_pred,epsilon,1-epsilon)
 	return - (y_true*np.log(y_pred)+(1-y_true)*np.log(1-y_pred))
+
+def training_func(X,y,learning_rate,iterations,reg_parameter):
+	m,n=X.shape
+	W = np.zeros(n)
+	b= 0
+
+	for _ in range(iterations):
+		z = np.dot(X,W) + b
+		A = 1/(1+np.exp(-z))
+
+		dW = (1/m)*np.dot(X.T,(A-y))+(reg_parameter/m)*W
+		db = np.mean(A-y)
+
+		W -= learning_rate*dW
+		b -= learning_rate*db
+	return W,b
+
