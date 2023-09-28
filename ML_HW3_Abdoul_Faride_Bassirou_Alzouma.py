04e99838f560e0514afe3aad1c26438e6cb55383
ABDOUL FARIDE BASSIROU ALZOUMA <abdoulfaridbassirou7898@gmail.com>
2023-09-28 17:41:02 +0300

    data preprocessing and Hot encoding

--- ML_HW3_Abdoul_Faride_Bassirou_Alzouma.py
+++ ML_HW3_Abdoul_Faride_Bassirou_Alzouma.py
@@ -5,3 +5,9 @@ import numpy as np
 
 mnist = keras.datasets.mnist
 (train_images,train_labels),(test_images,test_labels) = mnist.load_data()
+
+train_images,test_images = train_images/255.0,test_images/255.0
+
+train_labels = keras.utils.to_categorical(train_labels,10)
+test_labels = keras.utils.to_categorical(test_labels,10)
+
