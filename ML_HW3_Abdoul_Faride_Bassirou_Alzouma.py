b9d6e3ba1b95aa3fe62f14e4f58df5b258d03e3d
ABDOUL FARIDE BASSIROU ALZOUMA <abdoulfaridbassirou7898@gmail.com>
2023-09-28 17:32:45 +0300

    loading the MNIST Dataset

--- ML_HW3_Abdoul_Faride_Bassirou_Alzouma.py
+++ ML_HW3_Abdoul_Faride_Bassirou_Alzouma.py
@@ -2,3 +2,6 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
+
+mnist = keras.datasets.mnist
+(train_images,train_labels),(test_images,test_labels) = mnist.load_data()
