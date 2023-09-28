4af779c6ac6f137c6174d7c2b707088dc6afa6d5
ABDOUL FARIDE BASSIROU ALZOUMA <abdoulfaridbassirou7898@gmail.com>
2023-09-28 17:55:38 +0300

    custom dense layer

--- ML_HW3_Abdoul_Faride_Bassirou_Alzouma.py
+++ ML_HW3_Abdoul_Faride_Bassirou_Alzouma.py
@@ -11,3 +11,18 @@ train_images,test_images = train_images/255.0,test_images/255.0
 train_labels = keras.utils.to_categorical(train_labels,10)
 test_labels = keras.utils.to_categorical(test_labels,10)
 
+class CustomDenseLayer(layers.Layer):
+	def __init__(self,units,activation=None):
+		super(CustomDenseLayer,self).__init__()
+		self.units = units
+		self.activation=activation
+
+	def build(self,input_shape):
+		self.kernel = self.add_weight("kernel",(input_shape[-1],self.units))
+		self.bias = self.add_weight("bias",(self.units))
+
+	def call(self,inputs):
+		output = tf.matmul(inputs,self.kernel) + self.bias
+		if self.activation is not None:
+			output = self.activation(output)
+		return output
